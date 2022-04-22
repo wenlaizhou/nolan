0ed2ff577eb94803e9ccd6d3b45ae43e03428164
wenlai <wenlai_zhou@126.com>
2022-04-22 19:56:35 +0800

    create project

--- storage/data.go
+++ storage/data.go
@@ -13,10 +13,19 @@ type Master struct {
 
 // Task 任务
 type Task struct {
+	Slot   int          `json:"slot"`
+	Name   string       `json:"name"`
+	Status []TaskStatus `json:"status"`
+}
+
+// TaskStatus 任务状态
+type TaskStatus struct {
 }
 
 // Worker 工作节点
 type Worker struct {
-	Name string
-	Ip   string
+	Name     string          `json:"name"`
+	Ip       string          `json:"ip"`
+	TaskSlot int             `json:"task_slot"`
+	Tasks    map[string]Task `json:"tasks"`
 }
