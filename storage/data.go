b91e54b786d23af1ad9a96653ffd6f8fcad2df7a
wenlai <wenlai_zhou@126.com>
2022-04-22 20:05:31 +0800

    create project

--- storage/data.go
+++ storage/data.go
@@ -6,26 +6,32 @@ type Cluster struct {
 
 // Master 控制节点
 type Master struct {
-	Name     string `json:"name"`
-	TaskSlot int    `json:"task_slot"`
-	Ip       string
+	Name          string `json:"name"`
+	TotalTaskSlot int    `json:"totalTaskSlot"`
+	IdleTaskSlot  int    `json:"idleTaskSlot"`
+	Ip            string
 }
 
 // Task 任务
 type Task struct {
-	Slot   int          `json:"slot"`
-	Name   string       `json:"name"`
-	Status []TaskStatus `json:"status"`
+	Slot          int          `json:"slot"`
+	Name          string       `json:"name"`
+	CurrentStatue TaskStatus   `json:"currentStatue"`
+	Status        []TaskStatus `json:"status"`
 }
 
 // TaskStatus 任务状态
 type TaskStatus struct {
+	Log        []string `json:"log"`
+	CreateTime int      `json:"createTime"`
+	Statue     string   `json:"statue"`
 }
 
 // Worker 工作节点
 type Worker struct {
-	Name     string          `json:"name"`
-	Ip       string          `json:"ip"`
-	TaskSlot int             `json:"task_slot"`
-	Tasks    map[string]Task `json:"tasks"`
+	Name          string          `json:"name"`
+	Ip            string          `json:"ip"`
+	TotalTaskSlot int             `json:"totalTaskSlot"`
+	IdleTaskSlot  int             `json:"idleTaskSlot"`
+	Tasks         map[string]Task `json:"tasks"`
 }
