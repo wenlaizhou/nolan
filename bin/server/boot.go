97acc1424e9f430e09d4dd348f1389981a8c89bf
wenlai <wenlai_zhou@126.com>
2022-04-24 19:25:08 +0800

    create project

--- bin/server/boot.go
+++ bin/server/boot.go
@@ -3,5 +3,6 @@ package main
 import "nolan/server"
 
 func main() {
+
 	server.Boot()
 }
--- server/service.go
+++ server/service.go
@@ -10,10 +10,10 @@ func Boot() {
 
 	conf := middleware.LoadConfig("app.properties")
 
-	Swagger.AddPath(middleware.SwaggerBuildPath("", "", "", ""))
+	Swagger.AddPath(middleware.SwaggerBuildPath("/clusterMap", "", "GET", ""))
 	middleware.RegisterHandler("/clusterMap", func(context middleware.Context) {
 		if !checkAuth(conf, context.GetHeader("Authorization")) {
-			context.Error(404, "")
+			context.Error(403, "NO AUTHORITY")
 			return
 		}
 
@@ -25,6 +25,12 @@ func Boot() {
 
 	middleware.RegisterHandler("", func(context middleware.Context) {
 
+	})
+
+	middleware.EnableSwagger(Swagger)
+
+	middleware.RegisterIndex(func(context middleware.Context) {
+		context.Redirect("/swagger-ui")
 	})
 
 	middleware.StartServer("", conf.IntUnsafe("port"))
